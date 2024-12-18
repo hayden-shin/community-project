e9771d360e83a417eac47e7d25260b0b14ee8788
hayden <haydenhyo@gmail.com>
2024-12-18 12:45:26 +0900

    DB 연결 전 userModel es6

--- models/userModel.js
+++ models/userModel.js
@@ -1,16 +1,15 @@
-const fs = require('fs');
-const path = require('path');
-const usersFilePath = path.join(__dirname, '../data/users.json');
+import fs from 'fs';
+import path from 'path';
+
+const usersFilePath = path.join(process.cwd(), './data/users.json');
+
 // JSON 파일에서 사용자 데이터를 읽어오는 함수
-function readUsersFromFile() {
+export function readUsersFromFile() {
   const data = fs.readFileSync(usersFilePath, 'utf-8');
   return JSON.parse(data);
 }
+
 // JSON 파일에 사용자 데이터를 저장하는 함수
-function writeUsersToFile(users) {
+export function writeUsersToFile(users) {
   fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2), 'utf-8');
 }
-module.exports = {
-  readUsersFromFile,
-  writeUsersToFile,
-};
