1a7ca2f1dca2aa4f2e6d369891ecbd4d461c8ab6
hayden <haydenhyo@gmail.com>
2025-01-07 19:11:57 +0900

    refactor: correct CORS header to use localhost in server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(cookieParser());
 
 // OPTIONS 요청 예외처리
 app.use('*', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', `http://${SERVER_URL}:2000`);
+  res.header('Access-Control-Allow-Origin', `http://localhost:2000`);
   res.header(
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, DELETE, PATCH, OPTIONS'
