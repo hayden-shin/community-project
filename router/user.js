a036f1399f0e81fa065fcf3881f800e3840bb469
hayden <haydenhyo@gmail.com>
2025-01-11 16:34:09 +0900

    refactor: user namespace import for userController in user.js

--- router/user.js
+++ router/user.js
@@ -1,21 +1,16 @@
 import express from 'express';
-import {
-  getUserProfile,
-  updateProfile,
-  updatePassword,
-} from '../controller/user.js';
-
+import * as userController from '../controller/user.js';
 import { upload } from '../middleware/upload.js';
 
 const router = express.Router({ mergeParams: true });
 
 // 사용자 프로필 가져오기
-router.get('/profile', getUserProfile);
+router.get('/profile', userController.getUserProfile);
 
 // 사용자 정보 변경
-router.patch('/profile', upload, updateProfile);
+router.patch('/profile', upload, userController.updateProfile);
 
 // 비밀번호 변경
-router.patch('/password', updatePassword);
+router.patch('/password', userController.updatePassword);
 
 export default router;
