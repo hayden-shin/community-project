3aaef54ebddf3844629fbce3f78f4346d6a72d2c
hayden <haydenhyo@gmail.com>
2025-01-11 16:33:27 +0900

    refactor: use namespace import for postController in post.js

--- router/post.js
+++ router/post.js
@@ -1,39 +1,31 @@
 import express from 'express';
-import {
-  createPost,
-  getPostById,
-  getAllPosts,
-  editPost,
-  deletePost,
-  getLikeStatus,
-  toggleLikePost,
-} from '../controller/post.js';
+import * as postController from '../controller/post.js';
 import { upload } from '../middleware/upload.js';
 
 const router = express.Router({ mergeParams: true });
 
 // 새 게시글 생성
-router.post('/', upload, createPost);
+router.post('/', upload, postController.createPost);
 
 // 특정 게시글 가져오기
-router.get('/:post_id', getPostById);
+router.get('/:post_id', postController.getPostById);
 
 // 모든 게시글 가져오기
-router.get('/', getAllPosts);
+router.get('/', postController.getAllPosts);
 
 // 게시글 수정
-router.patch('/:post_id', upload, editPost);
+router.patch('/:post_id', upload, postController.editPost);
 
 // 게시글 삭제
-router.delete('/:post_id', deletePost);
+router.delete('/:post_id', postController.deletePost);
 
 // 좋아요 상태 확인
-router.get('/:post_id/likes', getLikeStatus);
+router.get('/:post_id/likes', postController.getLikeStatus);
 
 // 좋아요 추가
-router.post('/:post_id/likes', toggleLikePost);
+router.post('/:post_id/likes', postController.toggleLikePost);
 
 // 좋아요 취소
-router.delete('/:post_id/likes', toggleLikePost);
+router.delete('/:post_id/likes', postController.toggleLikePost);
 
 export default router;
