3523d537f97dd8548479098cd68d1307193c6983
hayden <haydenhyo@gmail.com>
2025-01-11 13:03:09 +0900

    refactor: use authController namespace in auth router

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,24 +1,18 @@
 import express from 'express';
-import {
-  signup,
-  login,
-  logout,
-  deleteAccount,
-} from '../controllers/userController.js';
+import * as authController from '../controllers/userController.js';
 import { upload } from '../middlewares/upload.js';
-
 const router = express.Router({ mergeParams: true });
 
 // 회원가입
-router.post('/signup', upload, signup);
+router.post('/signup', upload, authController.signup);
 
 // 로그인
-router.post('/login', login);
+router.post('/login', authController.login);
 
 // 로그아웃
-router.post('/logout', logout);
+router.post('/logout', authController.logout);
 
 // 회원 탈퇴
-router.delete('/account', deleteAccount);
+router.delete('/account', authController.deleteAccount);
 
 export default router;
