3c783573543aa88ca3a1ae53fcb752485090feef
hayden <haydenhyo@gmail.com>
2025-01-11 16:35:31 +0900

    refactor: use namespace import for commentController in comment.js

--- router/comment.js
+++ router/comment.js
@@ -1,23 +1,18 @@
 import express from 'express';
-import {
-  createComment,
-  getCommentsByPostId,
-  updateComment,
-  deleteComment,
-} from '../controller/comment.js';
+import * as commentController from '../controller/comment.js';
 
 const router = express.Router({ mergeParams: true });
 
 // 댓글 생성
-router.post('/', createComment);
+router.post('/', commentController.createComment);
 
 // 특정 게시글의 댓글 가져오기
-router.get('/', getCommentsByPostId);
+router.get('/', commentController.getCommentsByPostId);
 
 // 댓글 수정
-router.patch('/:comment_id', updateComment);
+router.patch('/:comment_id', commentController.updateComment);
 
 // 댓글 삭제
-router.delete('/:comment_id', deleteComment);
+router.delete('/:comment_id', commentController.deleteComment);
 
 export default router;
