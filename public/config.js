d12a1207f8e26cec955323bf887593fb81062867
hayden <haydenhyo@gmail.com>
2025-01-16 15:19:56 +0900

    feat: add CDN_URL

--- public/config.js
+++ public/config.js
@@ -1,3 +1,2 @@
-const BASE_URL = 'http://3.35.218.197:3000';
-
-export default BASE_URL;
+export const BASE_URL = 'http://3.35.218.197:3000';
+export const CDN_URL = 'https://d222sz9qo9ux2.cloudfront.net';
